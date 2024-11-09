1f5a1e3261eba901ded1683b69040d9aeeb8a657
crunux <joancruz0502@hotmail.com>
2024-11-09 10:43:47 -0400

    feat: add endpoit to search params paciente and return json

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,32 +1,36 @@
 # -*- coding: utf-8 -*-
+import json
 from odoo import http
 from odoo.http import request
 
 
 class VerticalHospital(http.Controller):
-    @http.route("/vertical_hospital", type="json", auth="public")
-    def index(self, secuencia, **kw):
-        # return (
-        #     request.env["vertical_hospital.paciente"]
-        #     .search([("secuencia", "=", secuencia)])
-        #     .read()
-        # )
+    @http.route(
+        "/vertical_hospital/",
+        type="http",
+        auth="public",
+    )
+    def index(self, **kw):
+        return "Hola, mundo"
 
-        return {
-            "message": "Hello, world",
+
+class Pacientes(http.Controller):
+    @http.route(
+        "/pacientes/consulta/<secuencia>",
+        type="http",
+        auth="public",
+        methods=["GET"],
+    )
+    def paciente(self, secuencia, **kw):
+        paciente = (
+            request.env["vertical_hospital.paciente"]
+            .search([("secuencia", "=", secuencia)])
+            .read()
+        )
+        output = {
+            "sec": paciente[0]["secuencia"],
+            "name": paciente[0]["nombre_completo"],
+            "rnc": paciente[0]["rnc"],
+            "state": paciente[0]["estado"],
         }
-
-
-#     @http.route('/vertical_hospital/vertical_hospital/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('vertical_hospital.listing', {
-#             'root': '/vertical_hospital/vertical_hospital',
-#             'objects': http.request.env['vertical_hospital.vertical_hospital'].search([]),
-#         })
-
-#     @http.route('/vertical_hospital/vertical_hospital/objects/<model("vertical_hospital.vertical_hospital"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('vertical_hospital.object', {
-#             'object': obj
-#         })
-
+        return json.dumps(output)
